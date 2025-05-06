133feba2686aa4314f0a4ee9466aa6dd9e19f19c
B4RC3N4 <ivan.barcenacagigas@alum.uca.es>
2025-05-07 00:28:21 +0200

    Add files via upload

---
+++ lleonartetal.R
@@ -0,0 +1,26 @@
+#estandarizacion alometrica Lleonart et al (2000)
+normalizar_medida <- function(data, Y_name, X_name, X0 = NULL, b = NULL) {
+  Y <- data[[Y_name]]
+  X <- data[[X_name]]
+  
+  if (is.null(X0)) {
+    X0 <- mean(X, na.rm = TRUE)
+  }
+  
+  if (is.null(b)) {
+    b <- coef(lm(log(Y) ~ log(X)))[2]  # Estima el exponente alométrico
+    modelo=lm(log(Y) ~ log(X))
+    print(summary(modelo))
+  }
+  
+  Y_norm <- Y * (X0 / X)^b
+  data[[paste0(Y_name, ".std")]] <- Y_norm
+  
+  return(data)
+}
+var_to_normallo=c('MO_mg','otolith.area','otolith.length','otolith.width','otolith.perimeter')
+for (var in var_to_normallo){
+  otolitos.data=
+    otolitos.data %>% group_by(Species)%>%
+    group_modify(~normalizar_medida(.x,Y_name = var,X_name = 'TL._mm')) %>%
+    ungroup()}
